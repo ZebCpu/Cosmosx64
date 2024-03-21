d1eca16c47e750d64cde3227eaa9689c947d01ec
ZebCPU <153043804+ZebCpu@users.noreply.github.com>
2024-03-21 03:46:09 +0000

    Update kernel.asm

--- Build/Test Kernel/kernel.asm
+++ Build/Test Kernel/kernel.asm
@@ -1,27 +1,31 @@
 org 0x1000000
-use32
+
+use64
+
 [map symbols]
 
 begin:
-_begin:
-	jmp entry
 
-align 4
-	
+_begin:
+
+jmp entry
+
+align 8
+
 multiboot_header:
-MULTIBOOT_HEADER_MAGIC dd 0x1BADB002
-MULTIBOOT_HEADER_FLAGS dd 0x00010003
-Checksum dd -(0x1BADB002 + 0x00010003)
-header_addr dd multiboot_header
-load_addr dd _begin
-load_end dd _end
-bss_end dd _end
-entry_val dd entry
+    MULTIBOOT_HEADER_MAGIC dq 0x1BADB002
+    MULTIBOOT_HEADER_FLAGS dq 0x00010003
+    Checksum dq -(0x1BADB002 + 0x00010003)
+    header_addr dq multiboot_header
+    load_addr dq _begin
+    load_end dq _end
+    bss_end dq _end
+    entry_val dq entry
 
 entry:
-	mov byte [0xB8000], 65
-	
+    mov byte [0xB8000], 65
+
 _loop:
-	jmp _loop
+    jmp _loop
 
 _end:
